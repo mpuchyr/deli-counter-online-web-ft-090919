fcf894cb17211f292352ec2239f20dc75c090541
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 23:22:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,11 @@ def take_a_number(line, name)
   puts "Welcome, #{name}. You are number #{line.index(name) + 1} in line."
   line
 end
+
+def now_serving(line)
+  if line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    
+  end
+end
