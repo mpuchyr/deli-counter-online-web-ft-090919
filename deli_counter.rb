369f6769453545c50fb8e3c45769270f5ffeeb1c
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 23:07:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,9 @@
 def line(queue)
   if queue.length == 0
     puts "The line is currently empty."
+  else
+    queue.each_with_index do |person, index|
+      puts "#{index}. #{person}"
   end
 end
 
