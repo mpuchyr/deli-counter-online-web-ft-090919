9d1216954cb89936319abef9289f32c666f0a43c
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 23:12:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,16 @@
 # Write your code here.
-
+line = "The line is currently"
 def line(queue)
   if queue.length == 0
-    puts "The line is currently empty."
+    line << " empty."
   else
-    print "The line is currently: "
+    line << ": "
     queue.each_with_index do |person, index|
-      print "#{index + 1}. #{person} "
+      line << "#{index + 1}. #{person} "
     end
   end
+  
+  line
 end
 
 def take_a_number
