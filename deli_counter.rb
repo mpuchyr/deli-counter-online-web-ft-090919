b57d82ed13e35a093f08bba08531bfb743107958
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-09 23:14:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,17 @@
 # Write your code here.
 
 def line(queue)
-  line = "The line is currently"
+  this_line = "The line is currently"
   if queue.length == 0
-    line << " empty."
+    this_line << " empty."
   else
-    line << ": "
+    this_line << ": "
     queue.each_with_index do |person, index|
-      line << "#{index + 1}. #{person} "
+      this_line << "#{index + 1}. #{person} "
     end
   end
   
-  line
+  puts this_line
 end
 
 def take_a_number
